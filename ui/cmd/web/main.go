a451b1e4acd9d49cf47b21ded53d35f4a1c680bd
kris <lamia.mortis@icloud.com>
2023-10-08 02:14:55 +0200

    [00000-0] Adding UI microservice structure

---
+++ ui/cmd/web/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"net/http"
+	"strings"
+
+	"ui/frontend"
+)
+
+func main() {
+	srv := &http.Server{
+		Addr:        ":8888",
+		Handler:     router(),
+	}
+
+	srv.ListenAndServe()
+}
+
+func router() http.Handler {
+	mux := http.NewServeMux()
+
+	// index
+	mux.HandleFunc("/", indexHandler)
+
+	// static files
+	staticFS, _ := fs.Sub(frontend.StaticFiles, "dist")
+	httpFS := http.FileServer(http.FS(staticFS))
+	mux.Handle("/static/", httpFS)
+
+	// some api
+	mux.HandleFunc("/api/v1/greeting", greetingAPI)
+	return mux
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, http.StatusText(http.StatusMethodNotAllowed))
+		return
+	}
+
+	if strings.HasPrefix(r.URL.Path, "/api") {
+		http.NotFound(w, r)
+		return
+	}
+
+	if r.URL.Path == "/favicon.ico" {
+		rawFile, _ := frontend.StaticFiles.ReadFile("dist/favicon.ico")
+		w.Write(rawFile)
+		return
+	}
+
+	rawFile, _ := frontend.StaticFiles.ReadFile("dist/index.html")
+	w.Write(rawFile)
+}
+
+func greetingAPI(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, there!"))
+}
